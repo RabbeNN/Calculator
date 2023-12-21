4271a7d44a5eb29fd067ebe8b45bbed08aa761b0
RabbeNN <rasmus.svensson2@gmail.com>
2023-12-21 09:55:02 +0100

    asd

--- Form1.cs
+++ Form1.cs
@@ -26,17 +26,20 @@ namespace Calculator
     public partial class Form1 : Form
     {
         public Form1()
+            //Välkomnar till kalkylatorn
         {
             InitializeComponent();
             textBox2.Text = "Välkommen till kalkylatorn";
         }
-
+        //Creates a variabel to the first number, the answer and to count the answer.
         float num1, ans;
         int count;
+        //Creates a list to get the history.
         List<string> lista = new List<string>();
         bool visarResultat = false;
         
-
+        //Every button for numbers have a if statement to clear the window if it contains the answer or "Ogiltlig inmatning". This is to be able to start a new
+        //calculation even if the window is not cleared manually.
         private void button1_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == ans.ToString() || textBox1.Text == "Ogiltlig inmatning")
@@ -176,12 +179,13 @@ namespace Calculator
             }
         }
 
+        //Erase button to clear the window.
         private void erasebtn_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
-            count = 0;
         }
 
+        //The if statement on the mathematic operators is used to add the operator if the window is not empty. If the window is empty u cant add the because it will give you error.
         private void plusbtn_Click(object sender, EventArgs e)
         {
             if (textBox1.Text != "") 
@@ -228,6 +232,8 @@ namespace Calculator
             }
         }
 
+        //The if statement on the decimal button is used because if the window is empty and you use the decimal it will give you "0," but if the window already have numbers
+        //it will only add the ","  
         private void btndecimal_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "")
@@ -240,7 +246,9 @@ namespace Calculator
             }
         
         }
-
+        //The if statement on the history button clears window 2 and reverse the list to get the most recent calculation on top.
+        //It adds the calculation to the list and creates a new line to get one calculation on new lines.
+        //If you press the button it will show you the history but if you press it again it will show you the welcome message.
         private void history_Click(object sender, EventArgs e)
         {
             if (!visarResultat)
@@ -261,6 +269,7 @@ namespace Calculator
             }
         }
 
+        //This is the switch case that calculates every mathematical operator. It gives you the answer of the two numbers you enter and then clears the box before you get the answer.
         public void compute(int count) 
         {
         switch (count)
@@ -300,6 +309,7 @@ namespace Calculator
                     break;
             }
         }
+        //This is the reset i use to reset before adding numbers.
         private void reset()
         {
             num1 = 0;
